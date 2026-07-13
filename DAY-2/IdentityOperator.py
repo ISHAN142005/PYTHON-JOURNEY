b3c473d1b093483f92a4b2e147b4257bd5f5b593
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-14 01:06:49 +0530

    Identity Operators

---
+++ DAY-2/IdentityOperator.py
@@ -0,0 +1,10 @@
+# Identity Operators
+
+list1 = ["Python", "Java"]
+list2 = list1
+list3 = ["Python", "Java"]
+
+print("list1 is list2:", list1 is list2)
+print("list1 is list3:", list1 is list3)
+
+print("list1 is not list3:", list1 is not list3)
