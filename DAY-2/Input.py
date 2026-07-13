9c442b374c3a31907ea9e91b5f0316f2b3afecaf
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-13 23:48:34 +0530

    UserInput

---
+++ DAY-2/Input.py
@@ -0,0 +1,19 @@
+num1 = input("Enter first number:")  #It will take input as a string (Drfault)
+num1=int(num1)
+
+num2 = input("Enter second number:")  #It will take input as a string (Drfault)
+num2=int(num2)
+
+sum =(num1 + num2)
+a=print("The sum of both the number is :",sum)
+
+
+
+print("-----------------------------------")
+num1 = input("Enter first number:")  
+num2 = input("Enter second number:")  
+
+sum =(num1 + num2)
+a=print("The sum of both the number is :",sum) #This will give output as num1num2 not num1+num2
+
+
