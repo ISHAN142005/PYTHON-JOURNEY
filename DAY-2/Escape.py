877d2fd4f17407d3243467f03813c71031fac3fa
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-14 00:19:56 +0530

    Escpace Characters

---
+++ DAY-2/Escape.py
@@ -0,0 +1,19 @@
+#By Default sep=space
+#By Default end=next line
+#For using \ use i t twice \\
+#for using "" use \""
+#for using ' use \'
+
+print("How are you? \nI am Good")
+
+print("Hello","Hii")
+
+print("Hello","Hii",sep="..")
+print("Hello","Hii",sep="#")
+
+print("New Session",end="..")  #Now as we declared end as .. we are not proceeding to next line .. is being printed
+print("Lets Goo")
+
+
+#-->Used for single line comment
+# ''' '''-->Used for multi line comment
