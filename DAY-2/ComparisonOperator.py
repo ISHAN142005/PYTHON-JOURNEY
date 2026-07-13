406ef70e510e68514f082727e16ef01418436ce4
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-14 01:02:52 +0530

    Comparison Operators

---
+++ DAY-2/ComparisonOperator.py
@@ -0,0 +1,11 @@
+# Comparison Operators
+
+age = 20
+minimum_age = 18
+
+print("Equal:", age == minimum_age)
+print("Not Equal:", age != minimum_age)
+print("Greater Than:", age > minimum_age)
+print("Less Than:", age < minimum_age)
+print("Greater Than or Equal To:", age >= minimum_age)
+print("Less Than or Equal To:", age <= minimum_age)
