4845740f2d8e8736b705950f8809fb7584f72f96
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-14 01:02:05 +0530

    Arithmetic Operators

---
+++ DAY-2/ArithmeticOperators.py
@@ -0,0 +1,12 @@
+# Arithmetic Operators
+
+num1 = 15
+num2 = 4
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Modulus (Remainder):", num1 % num2)
+print("Exponentiation:", num1 ** num2)
+print("Floor Division:", num1 // num2)
