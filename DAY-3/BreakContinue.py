264c53bf65e6917ffc6a8991331e67ac82eb9e00
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-14 22:23:36 +0530

    Break Continue Pass Statement

---
+++ DAY-3/BreakContinue.py
@@ -0,0 +1,13 @@
+for a in range(1, 21):
+    if a == 14:
+        continue
+    print(a, end=" ")
+
+
+print("\n")
+
+
+for i in range(1, 100):
+    print(i, end=" ")
+    if i == 33:
+        break
---
+++ DAY-3/PassStatement.py
@@ -0,0 +1,4 @@
+for i in range(5):
+    if i == 3:
+        pass  # Do Nothing  #It is used whwn we are willing to write afterwards not now
+    print(i, end=" ")  # Output: 0 1 2 3 4
