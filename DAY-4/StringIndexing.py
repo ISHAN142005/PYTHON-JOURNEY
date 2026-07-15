5539867145ecdd4d0b3d4e3365a3eb2658b1827f
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-15 15:59:42 +0530

    String Indexing

---
+++ DAY-4/StringIndexing.py
@@ -0,0 +1,26 @@
+Name="Ishan"
+Name1='ISHAN'
+Name='''Ishan is in the second year of btech
+        at the NorthCap University.'''          #MultiLine String
+
+'''
+ISHAN
+ 0  1  2   3   4  
+-5 -4 -3  -2  -1
+'''
+
+
+
+#Indexing
+print(Name[0])
+print(Name[1])
+print(Name[2])
+print(Name[3])
+print(Name[4])
+
+#Reverse Indexing
+print(Name[-1])
+print(Name[-2])
+print(Name[-3])
+print(Name[-4]) #Name[-4+5] Name[1]
+print(Name[-5])
