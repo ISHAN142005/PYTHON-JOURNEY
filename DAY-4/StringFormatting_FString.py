b67c1ccd7cb3bda34624be1bf420d6eb9ca28142
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-16 00:15:50 +0530

    STRING FORMATTING f-STRING CHARACTER ENCODING

---
+++ DAY-4/StringFormatting_FString.py
@@ -0,0 +1,38 @@
+# STRING FORMATTING
+print(
+    "*****************************************************************************************"
+)
+print("STRING FORMATTING")
+template = "Dear {},You are awesome.This is you reward--> {}"
+# print("Dear {},You are awesome.This is you reward--> {}".format(name,age))
+a = "Ishan"
+a1 = "1CR"
+b = "Sam"
+b1 = "Headphone"
+c = "Harry"
+c1 = "Phone"
+
+s1 = template.format(a, a1)
+print(s1)
+
+
+print(
+    "*****************************************************************************************"
+)
+print("f-String-")
+
+print(f"{a} you are awesome.This is your reward-->{a1}")
+
+print(
+    "*****************************************************************************************"
+)
+
+# CHARACTER ENCODING
+
+print("CHARACTER ENCODING")
+print(ord("A"))  # Output:65
+print(chr(65))  # Output:'A'
+
+print(
+    "*****************************************************************************************"
+)
