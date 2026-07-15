4ddd021f7a4a59c468502278c78bb1f388f31126
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-15 16:09:30 +0530

    String Slicing

---
+++ DAY-4/StringSlicing.py
@@ -0,0 +1,11 @@
+Name = "Ishan0123456789"
+print(Name[0:5])  # -->means print 0 to 5-1 ie 0 to 4
+print(Name[0:-5])  # -->Means print 0 to -5+15-1 ie 0 to 9
+
+# print(name[0:10:n]) -->skips n-1 character
+print(Name[0:10:1])  # -->skips 1-1 character
+print(Name[0:10:2])  # -->skips 2-1 character
+print(Name[0:10:3])  # -->skips 3-1 character
+
+print(Name[:4])  # Replace the first empty number with 0 i.e Name[0:4]
+print(Name[0:])  # Replace the last empty number with length i.e Name[0:15]
