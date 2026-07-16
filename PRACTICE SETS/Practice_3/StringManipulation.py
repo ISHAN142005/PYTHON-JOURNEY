edb34692fe4c2387474232aa7d095cbfc1de72e0
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-16 16:17:15 +0530

    String Manipulation Que

---
+++ PRACTICE SETS/Practice_3/StringManipulation.py
@@ -0,0 +1,12 @@
+"""
+Given sentence = "Coding in Python is fun", replace "fun" with "awesome" and print it.
+Find the index of the word "Python" in sentence.
+Convert the entire sentence to uppercase and print it.
+
+"""
+
+sentence = "Coding in Python is fun"
+print(sentence.replace("fun", "awesome"))
+
+print("Index of'Python' is:", sentence.find("Python"))
+print(sentence.upper())
