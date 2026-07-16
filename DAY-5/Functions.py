1f4d7d17f29c054122d8ecb4ad1a1a6e45ad6846
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-17 00:03:25 +0530

    Function Generation and its use

--- DAY-5/Functions.py
+++ DAY-5/Functions.py
@@ -17,10 +17,16 @@ n1 = int(input("Enter the first number here:"))
 n2 = int(input("Enter the second number here:"))
 n3 = int(input("Enter the third number here:"))
 
-print(f"The average of {n1},{n2},{n3} is:{avg(n1,n2,n3)}")      #avg(n1,n2,n3)-->Function call
+print(
+    f"The average of {n1},{n2},{n3} is:{avg(n1,n2,n3)}"
+)  # avg(n1,n2,n3)-->Function call
 
-a1=avg(2,3,4)
-a2=avg(4,7,9)
+a1 = avg(2, 3, 4)
+a2 = avg(4, 7, 9)
 
-print(a1)       #we used return in our function but if we had used print d then it would give output as none
-print(a2)       #we used return in our function but if we had used print d then it would give output as none
+print(
+    a1
+)  # we used return in our function but if we had used print d then it would give output as none
+print(
+    a2
+)  # we used return in our function but if we had used print d then it would give output as none
