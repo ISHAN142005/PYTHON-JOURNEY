0a4f05273b12b835b418e296be83455eb6e3add4
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-19 00:21:33 +0530

    2

---
+++ PRACTICE SETS/Practice_4/1/2.py
@@ -0,0 +1,9 @@
+# Write a function square(num) that returns the square of a given number. Test it with different numbers.
+
+
+def square(num):
+    return num * num
+
+
+a = int(input("Enter the number here:"))
+print("The square of ", a, "is:", square(a))
