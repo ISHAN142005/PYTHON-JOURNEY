97f0f38161b8ac75c44debc907f4cde4b7d4a13e
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-19 22:47:04 +0530

    Sets Introduction

---
+++ DAY-6/Sets.py
@@ -0,0 +1,5 @@
+#Sets are unordered, unique collections (no duplicates).
+Companies={"Google","Microsoft","Apple","Meta","Amazon"}
+
+print(Companies,type((Companies)))
+#print(Companies[3]) #-->As this is unordered this type of operations are not possible
