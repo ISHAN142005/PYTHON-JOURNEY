b54d2e6c4d603e10116c546c2e294141c8eb5335
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-19 22:26:55 +0530

    Tuple Unpacking

---
+++ DAY-6/TupleUnpacking.py
@@ -0,0 +1,9 @@
+#Tuple unpacking in Python means breaking a tuple into individual variables in a single assignment.
+
+Name=('Ishan','Harry','Sita')
+a,b,c=Name  #-->Tuple unpacking
+print(a,b,c)
+print(a)
+print(b)
+print(c)
+print(Name)
