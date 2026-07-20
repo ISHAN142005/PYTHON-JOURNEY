60f628843d1a505a7c57d04f55a24fafbd5ed25b
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-20 22:52:24 +0530

    Basic List Que 1

---
+++ PRACTICE SETS/Practice_5/Q1.py
@@ -0,0 +1,14 @@
+'''
+Create a list fruits = ["apple", "banana", "cherry"].
+
+Print the first fruit.
+Replace "banana" with "orange".
+Print the length of the list.
+'''
+
+fruit=["apple", "banana", "cherry"]
+print(fruit)
+print(fruit[0]) #First Fruit
+fruit[1]="orange"
+print(fruit)
+print(len(fruit))
