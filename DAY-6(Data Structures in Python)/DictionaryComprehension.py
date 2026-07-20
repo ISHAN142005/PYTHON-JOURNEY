7b1c45cd1fa68dc10abdecbfb1b45920f3606bb2
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-20 22:08:05 +0530

    Dictionary Comprehension & Data Structure Usage

---
+++ DAY-6(Data Structures in Python)/DictionaryComprehension.py
@@ -0,0 +1,17 @@
+table_of_5 = {x: x * 5 for x in range(1, 11)}
+print(table_of_5)
+
+squares = {x: x**2 for x in range(11)}
+print(squares)
+
+
+"""
+When to Use Each Data Structure?
+
+Data Structure	     Features	                  Best For
+List	          Ordered, Mutable	     Storing sequences, dynamic data
+Tuple	          Ordered, Immutable	 Fixed collections, dictionary keys
+Set            	  Unordered, Unique	     Removing duplicates, set operations
+Dictionary	      Key-Value Pairs	     Fast lookups, structured data
+
+"""
