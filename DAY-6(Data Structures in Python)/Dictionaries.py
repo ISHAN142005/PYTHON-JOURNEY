34267ff1b0d0836f3bb547e1cab393d87294ae38
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-20 21:47:34 +0530

    Dictionary Introduction

---
+++ DAY-6(Data Structures in Python)/Dictionaries.py
@@ -0,0 +1,8 @@
+# Dictionaries store key-value pairs and allow fast lookups.
+
+marks = {"Harry": 34, "Sam": 45, "Ishan": 98}
+# print(marks,type(marks)) --> Way of printing  Type is dic
+print(marks["Harry"])
+print(marks["Sam"])
+marks["Ishan"] = 100
+print(marks["Ishan"])
