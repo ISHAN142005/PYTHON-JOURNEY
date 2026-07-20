bbd11d858d46a7e8e4b83e37c1bb1db2e10ab37c
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-20 23:35:35 +0530

    Tuple Que

---
+++ PRACTICE SETS/Practice_5/3/Q1.py
@@ -0,0 +1,17 @@
+"""
+Create a tuple coordinates = (10, 20) and print both elements.
+"""
+
+coordinates = (10, 20)
+print(coordinates[0])
+print(coordinates[1])
+
+# Try to modify the tuple by setting coordinates[0] = 50 — note what happens.
+# coordinates[0]=50   Not Possible
+
+# Convert the tuple to a list, change its first element to 50, and convert it back to a tuple
+
+corlist = list(coordinates)
+corlist[0] = 50
+coordinates = tuple(corlist)
+print(coordinates)
