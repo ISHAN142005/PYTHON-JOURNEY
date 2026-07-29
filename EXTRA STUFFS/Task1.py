fa52fefaa14bcd3134fcea2eac39a7da2835a0d1
ISHAN142005 <ishanbohra1414@gmail.com>
2026-07-29 21:22:58 +0530

    Turtle Graphics Canvas

---
+++ EXTRA STUFFS/Task1.py
@@ -0,0 +1,23 @@
+import turtle
+import colorsys
+
+p = turtle.Turtle()
+s = turtle.Screen()
+s.title("Colorful Starburst Symphony")
+s.bgcolor("black")
+p.pensize(2.0)
+p.speed(0)
+
+n = 36
+h = 0.5
+
+for i in range(72):
+    c = colorsys.hsv_to_rgb(h, 1, 1)
+    h += 1 / n
+    p.color(c)
+    p.left(5)
+    for j in range(5):
+        p.forward(250)
+        p.left(144)
+
+turtle.done()
