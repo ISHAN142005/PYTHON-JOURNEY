6db568cec6b9133fda928ee501c2c3c9b48910f1
ISHAN142005 <ishanbohra1414@gmail.com>
2026-08-10 15:39:44 +0530

    ASSIGNMENT 5

---
+++ COLLEGE LAB/ASSIGNMENT5.py
@@ -0,0 +1,10 @@
+import math
+
+num = float(input("Enter a number: "))
+
+print("Log:", math.log(num))
+print("Sin:", math.sin(num))
+print("Cos:", math.cos(num))
+print("Absolute value:", abs(num))
+print("Square root:", math.sqrt(num))
+print("Power:", math.pow(num, 2))
