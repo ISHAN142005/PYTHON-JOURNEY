2d7516d08954801a5b32fbc9d6c579c833a87dd9
ISHAN142005 <ishanbohra1414@gmail.com>
2026-09-05 12:31:52 +0530

    ASSIGNMENT 10

---
+++ COLLEGE LAB/ASSIGNMENT10.py
@@ -0,0 +1,8 @@
+core_parts = ("Processor", "Mainboard", "Memory", "Graphics Card")
+print("Core parts:", core_parts)
+
+extras = ("Screen", "Typing Pad", "Pointer")
+print("Extras:", extras)
+
+full_setup = core_parts + extras
+print("\nComplete setup:", full_setup)
