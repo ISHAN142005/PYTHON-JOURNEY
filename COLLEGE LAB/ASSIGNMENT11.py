4c38a681b94efde1270ac1e43dbb6e073821a623
ISHAN142005 <ishanbohra1414@gmail.com>
2026-09-05 14:30:27 +0530

    ASSIGNMENT 11

---
+++ COLLEGE LAB/ASSIGNMENT11.py
@@ -0,0 +1,8 @@
+car_info = {"maker": "Manza", "model": "Model S", "year": 2015}
+print("Car details:", car_info)
+
+car_model = car_info["model"]
+print("\nChosen model:", car_model)
+
+car_info["year"] = 2026
+print("\nUpdated car details:", car_info)
